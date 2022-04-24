19f2897e3157a0a4338198a90f92fadd1958b694
AlvaroCastilloR <acastillor2016@inf.udec.cl>
2022-04-23 20:35:31 -0400

    conexion a email

---
+++ email_Conect/emailSend.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from flask_mail import Mail, Message 
+
+app = Flask(__name__)
+
+app.config['MAIL_SERVER']='smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = 'alvaro.castillo.rifo@gmail.com'
+app.config['MAIL_PASSWORD'] = 'isfiwsphwejadstk'
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+mail = Mail(app)
+
+@app.route("/")
+def hello():
+  msg = Message('Hello from the other side!', sender =   'alvaro.castillo.rifo@gmail.com', recipients = ['franico_c@hotmail.com','fcarrion2018@udec.cl','fcarrion2018@inf.ude.cl'])
+  msg.body = "HOLA FRANO ESTYO PROBANDOOOOO!"
+  mail.send(msg)
+  return "Message sent!"
