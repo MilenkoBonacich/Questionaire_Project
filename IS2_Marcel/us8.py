e7af1e19e6f1868fa1257a6023a1cdae893dee0c
LecramC4 <marcelsonic@gmail.com>
2022-04-26 01:31:52 -0400

    US7/US8 listas, mas conexion con DB

--- IS2_Marcel/us8.py
+++ IS2_Marcel/us8.py
@@ -1,14 +1,61 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, redirect, url_for, request
+
+import psycopg2
 import forms
 
 app = Flask(__name__)
 
+def get_dbconnection():
+    PSQL_HOST = "ec2-52-201-124-168.compute-1.amazonaws.com"
+    PSQL_PORT = "5432"
+    PSQL_USER = "hppcxweyblynah"
+    PSQL_PASS = "e4dbaf0502a56b15b4a64151940508613171a9e287817e5a4417d292fa8eb5ec"
+    PSQL_DB = "de3skgdka01pk8"
+    connstr = "host=%s port=%s user=%s password=%s dbname=%s" % (PSQL_HOST, PSQL_PORT, PSQL_USER, PSQL_PASS, PSQL_DB)
+    conn = psycopg2.connect(connstr)
+    return conn
+
 @app.route("/")
 def email_register():
 
-	email = forms.emailForm()
-	return render_template('structure.html', eTF=email)
+	emailTF = forms.emailForm()
+	return render_template('email_Register.html', title='Registration', eTF=emailTF)
+
+
+@app.route("/list")
+def email_list():
+	
+	conn = get_dbconnection()
+	cur = conn.cursor()
+    
+	sqlquery = "select * from email;"
+	cur.execute(sqlquery)
+	query = cur.fetchall()
+	length = len(query)
+    
+	cur.close()
+	conn.close()
+
+	return render_template('email_List.html', title='Email DataBase', emails=query, index=length )
+
+
+@app.route('/email')
+def userEmail():
+
+	email = request.args.get('email', 'no email')
+
+	conn = get_dbconnection()
+	cur = conn.cursor()
+	
+	sqlquery = """ INSERT INTO email (id) VALUES ('%s') """ %(email)
+	cur.execute( sqlquery )
+	conn.commit()
+
+	cur.close()
+	conn.close()
+
+	return render_template('email_Confirm.html', title='Registration Successful', email=email)
 
 if __name__ == '__main__':
     app.run( debug=True )
