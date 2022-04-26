438743350d72e105a2acce464ff2eca6b9e93dd2
FranoCar <franico_c@hotmail.com>
2022-04-26 13:35:05 -0400

    Interfaz para responder encuestas

---
+++ responder/main.py
@@ -0,0 +1,77 @@
+from flask import Flask, request, render_template, redirect, url_for
+import psycopg2
+
+
+app = Flask(__name__)
+
+
+def get_dbconnection():
+    PSQL_HOST = "ec2-52-201-124-168.compute-1.amazonaws.com"
+    PSQL_PORT = "5432"
+    PSQL_USER = "hppcxweyblynah"
+    PSQL_PASS = "e4dbaf0502a56b15b4a64151940508613171a9e287817e5a4417d292fa8eb5ec"
+    PSQL_DB = "de3skgdka01pk8"
+    connstr = "host=%s port=%s user=%s password=%s dbname=%s" % (PSQL_HOST, PSQL_PORT, PSQL_USER, PSQL_PASS, PSQL_DB)
+    conn = psycopg2.connect(connstr)
+    return conn
+
+@app.route('/')
+def home(): #Página con cuadro de texto para consultar por un id de encuesta
+    return render_template('answers.html')
+
+@app.route('/', methods={'POST'})   #Método POST para procesar form, se redirige a la url para respuestas con la id correspondiente.
+def redirecting():
+    text = request.form['u']
+    return redirect(url_for('responder',id_e=text))
+
+@app.route('/encuesta/<string:id_e>/responder')    #Url con la lista, tiene la id de encuesta en la url
+def responder(id_e):
+    conn = get_dbconnection()                       #Conexión a la base de datos
+    #Seleccionar titulo de la encuesta
+    cur = conn.cursor()
+    sqlquery = "select * from encuesta where encuesta.id_e = \'" + id_e + "\';"
+    cur.execute(sqlquery)
+    row = cur.fetchone()            #Acceso a los datos de la encuesta
+
+    if row is None:                 #Si no se obtiene nada se retorna error.
+        return {"Error":"No existe esta encuesta :("}
+
+    title = row[1]                  #Título de la encuesta
+    cur.close()                     #Se cierra el cursor
+    #Seleccionar preguntas de la encuesta
+    cur = conn.cursor()             #Se reabre el cursor
+    sqlquery = "select * from pregunta where pregunta.id_e = \'" + id_e + "\';" #Arreglo de preguntas
+    cur.execute(sqlquery)
+    preguntas = cur.fetchall()
+    cur.close()
+    cur = conn.cursor()
+    sqlquery =  """
+                select *
+                from alternativa a, (select p.id_p from pregunta p where p.id_e = \'""" + id_e + """\') as b
+                where a.id_p = b.id_p;
+                """
+    cur.execute(sqlquery)
+    alternativas = cur.fetchall()   #Arreglo de alternativas
+    cur.close()
+    conn.close()
+    return render_template('formulario.html', title=title, preguntas=preguntas, alternativas=alternativas)
+
+@app.route('/encuesta/<string:id_e>/responder', methods={'POST'})
+def enviar(id_e):
+    conn = get_dbconnection()
+    cur = conn.cursor()
+
+    opciones = list(request.form.keys())
+    opciones.remove("enviar_respuesta")
+    resp = []
+    for op in opciones:
+        resp.append(request.form[op])
+    for r in resp:
+        query = """ UPDATE alternativa
+                    SET cont_r = cont_r+1
+                    WHERE id_a = \'""" + r + "\'"
+        cur.execute(query)
+    conn.commit()
+    cur.close()
+    conn.close()
+    return {"Greacias! :D":"Agradecemos su participación"}
