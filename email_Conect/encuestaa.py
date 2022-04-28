6826fe69042e71a33bdd48380118210b6e4c4e81
FranoCar <franico_c@hotmail.com>
2022-04-27 23:47:41 -0400

    Integración lista de respuestas con lo del álvaro y mauri

--- email_Conect/encuestaa.py
+++ email_Conect/encuestaa.py
@@ -124,6 +124,40 @@ def previewE(id_e):
     conn.close()
     return render_template('previewE.html', titulo=title, preguntas=preguntas, alternativas=alternativas)
 
+#Código Franco ---------------------------------------------------------------------------------------------
+@app.route('/encuesta/<string:id_e>/respuestas')    #Url con la lista, tiene la id de encuesta en la url
+def respuestas(id_e):
+    conn = get_dbconnection()                       #Conexión a la base de datos
+    #Seleccionar titulo de la encuesta
+    cur = conn.cursor()
+    sqlquery = "select * from encuesta where encuesta.id_e = \'" + id_e + "\';"
+    cur.execute(sqlquery)
+    row = cur.fetchone()            #Acceso a los datos de la encuesta
+
+    if row is None:                 #Si no se obtiene nada se retorna error.
+        return {"Error":"No existe esta encuesta :("}
+
+    title = row[1]                  #Título de la encuesta
+    cur.close()                     #Se cierra el cursor
+    #Seleccionar preguntas de la encuesta
+    cur = conn.cursor()             #Se reabre el cursor
+    sqlquery = "select * from pregunta where pregunta.id_e = \'" + id_e + "\';" #Arreglo de preguntas
+    cur.execute(sqlquery)
+    preguntas = cur.fetchall()
+    cur.close()
+    cur = conn.cursor()
+    sqlquery =  """
+                select *
+                from alternativa a, (select p.id_p from pregunta p where p.id_e = \'""" + id_e + """\') as b
+                where a.id_p = b.id_p;
+                """
+    cur.execute(sqlquery)
+    alternativas = cur.fetchall()   #Arreglo de alternativas
+    cur.close()
+    conn.close()
+    return render_template('list.html', title=title, preguntas=preguntas, alternativas=alternativas)
+#-----------------------------------------------------------------------------------------------------------------
+
 # @app.route("/previewE/<param>/")
 # def preview(param):
    
